36b965b42e179c751a52f575fe7c2d601b45a081
Kabeer Ahamed <kabeerahamedkm@gmail.com>
2024-07-24 13:28:51 +0530

    e-commerce assignment

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css';
+import Structure from './pages/structure';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Structure />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
+
